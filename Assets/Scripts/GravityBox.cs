97081e55d75df69ee6530030b886c4869300f237
Carlos Dominguez <dominguerilla@gmail.com>
2022-01-08 20:32:15 -0500

    4.4; draw outer cube boundaries

--- Assets/Scripts/GravityBox.cs
+++ Assets/Scripts/GravityBox.cs
@@ -13,7 +13,10 @@ public class GravityBox : GravitySource
     [SerializeField, Min(0f)]
     float innerRadius = 0f, innerFalloffDistance = 0f;
 
-    float innerFalloffFactor;
+    [SerializeField, Min(0f)]
+    float outerDistance = 0f, outerFalloffDistance = 0f;
+
+    float innerFalloffFactor, outerFalloffFactor;
 
     private void Awake()
     {
@@ -29,7 +32,9 @@ public class GravityBox : GravitySource
             Mathf.Min(innerFalloffDistance, maxInner),
             innerRadius
         );
+        outerFalloffDistance = Mathf.Max(outerFalloffDistance, outerDistance);
         innerFalloffFactor = 1f / (innerFalloffDistance - innerRadius);
+        outerFalloffFactor = 1f / (outerFalloffDistance - outerDistance);
     }
 
     private void OnDrawGizmos()
@@ -50,7 +55,74 @@ public class GravityBox : GravitySource
             Gizmos.color = Color.cyan;
             Gizmos.DrawWireCube(Vector3.zero, innerFalloffDimensions * 2f);
         }
+        if(outerDistance > 0f)
+        {
+            Gizmos.color = Color.yellow;
+            DrawGizmosOuterCube(outerDistance);
+        }
+        if(outerFalloffDistance > outerDistance)
+        {
+            Gizmos.color = Color.cyan;
+            DrawGizmosOuterCube(outerFalloffDistance);
+        }
+    }
 
+    void DrawGizmosRect(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+    {
+        Gizmos.DrawLine(a, b);
+        Gizmos.DrawLine(b, c);
+        Gizmos.DrawLine(c, d);
+        Gizmos.DrawLine(d, a);
+    }
+
+    void DrawGizmosOuterCube(float distance)
+    {
+        Vector3 a, b, c, d;
+
+        // Draw the right face (+X)
+        a.y = b.y = boundingVector.y;
+        d.y = c.y = -boundingVector.y;
+        b.z = c.z = boundingVector.z;
+        d.z = a.z = -boundingVector.z;
+        a.x = b.x = c.x = d.x = boundingVector.x + distance;
+        DrawGizmosRect(a, b, c, d);
+
+        // Draw the left face (-X)
+        a.x = b.x = c.x = d.x = -a.x;
+        DrawGizmosRect(a, b, c, d);
+
+        // Draw the top face (+Y)
+        a.x = d.x = boundingVector.x;
+        b.x = c.x = -boundingVector.x;
+        a.z = b.z = boundingVector.z;
+        c.z = d.z = -boundingVector.z;
+        a.y = b.y = c.y = d.y = boundingVector.y + distance;
+        DrawGizmosRect(a, b, c, d);
+
+        // Draw the bottom face (-Y)
+        a.y = b.y = c.y = d.y = -a.y;
+        DrawGizmosRect(a, b, c, d);
+
+        // Draw the forward face (+Z)
+        a.y = b.y = boundingVector.y;
+        c.y = d.y = -boundingVector.y;
+        a.x = d.x = -boundingVector.x;
+        b.x = c.x = boundingVector.x;
+        a.z = b.z = c.z = d.z = boundingVector.z + distance;
+        DrawGizmosRect(a, b, c, d);
+
+        // Draw the toward face (-Z)
+        a.z = b.z = c.z = d.z = -a.z;
+        DrawGizmosRect(a, b, c, d);
+
+        // this is the sqrt of 1/3; we are offsetting each boundary point in all three dimensions equally
+        // TODO: study the math. Might have to do with Pythagorean's Theorem, but in 3 dimensions?
+        distance *= 0.5773502692f;
+        Vector3 size = boundingVector;
+        size.x = 2f * (boundingVector.x + distance);
+        size.y = 2f * (boundingVector.y + distance);
+        size.z = 2f * (boundingVector.z + distance);
+        Gizmos.DrawWireCube(Vector3.zero, size);
     }
 
     float GetGravityComponent(float coordinate, float distance)
