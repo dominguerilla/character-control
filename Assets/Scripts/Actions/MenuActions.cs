2b96b47d89be056fe7f2781208f04d13c2024567
Carlos Dominguez <dominguerilla@gmail.com>
2022-01-12 17:15:10 -0500

    Add PlayerActions, start pause menu prefab

---
+++ Assets/Scripts/Actions/MenuActions.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class MenuActions : PlayerAction
+{
+
+    public UnityEvent onPause = new UnityEvent(), onUnpause = new UnityEvent();
+
+    [SerializeField]
+    GameObject pauseUI;
+
+    bool isPaused;
+    bool canPause = true;
+
+    public override void Register(PlayerControls controls)
+    {
+        if (!isRegistered)
+        {
+            base.Register(controls);
+            controls.Gameplay.Menu1.performed += TogglePause;
+            controls.UI.Menu1.performed += TogglePause;
+        }
+    }
+
+    public override void Unregister()
+    {
+        if (isRegistered)
+        {
+            base.Unregister();
+            controls.Gameplay.Menu1.performed -= TogglePause;
+            controls.UI.Menu1.performed -= TogglePause;
+        }
+    }
+
+    public void TogglePause(InputAction.CallbackContext ctx)
+    {
+        if (!isPaused && !canPause) return;
+
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+
+        pauseUI.SetActive(isPaused);
+
+        if (isPaused)
+        {
+            controls.Gameplay.Disable();
+            controls.UI.Enable();
+            onPause.Invoke();
+        }
+        else
+        {
+            controls.UI.Disable();
+            controls.Gameplay.Enable();
+            onUnpause.Invoke();
+        }
+
+    }
+
+    // Dumb hack I have to do in order to hook up Unity UI buttons to this function
+    public void TogglePause()
+    {
+        TogglePause(new InputAction.CallbackContext());
+    }
+
+    /// <summary>
+    /// Enables/disables pausing. 
+    /// 
+    /// </summary>
+    /// <param name="value">true to allow pausing, false otherwise.</param>
+    public void SetCanPause(bool value)
+    {
+        canPause = value;
+    }
+
+    private void OnDestroy()
+    {
+        Unregister();
+    }
+    
+}
---
+++ Assets/Scripts/Actions/PlayerAction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAction : MonoBehaviour
+{
+    protected PlayerControls controls;
+    protected bool isRegistered;
+    public virtual void Register(PlayerControls controls)
+    {
+        this.controls = controls;
+        this.isRegistered = true;
+    }
+
+    public virtual void Unregister()
+    {
+        this.controls = null;
+        this.isRegistered = false;
+    }
+}
+
+
